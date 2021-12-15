7dd5b3552f6c6c2eeef90d8b64664ffcb42efda6
dmholtz <56723830+dmholtz@users.noreply.github.com>
2021-12-15 17:56:39 +0100

    Add mock implementation

---
+++ index.js
@@ -0,0 +1,19 @@
+handle_json = function (json) {
+    return json;
+}
+
+exports.calc = function (req, res) {
+    let content_type = req.get('content-type');
+
+    switch (content_type) {
+        case 'application/json':
+            let req_json = req.body;
+            let res_json = handle_json(req_json);
+            res.status(200).send(res_json);
+            break;
+        default:
+            res.status(300).send("Cannot handle request");
+            break;
+    }
+
+};
